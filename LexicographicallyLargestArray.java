7c9881f1d3ab300db4f70a7861cf82bf3926568d
PAWAN KUMAR <40031829+Perseverant@users.noreply.github.com>
2021-01-09 18:33:24 +0530

    Create LexicographicallyLargestArray.java

---
+++ LexicographicallyLargestArray.java
@@ -0,0 +1,45 @@
+
+/***   INTERVIEWBIT CONTEST RPBLEM SOLUTION.. PRBLEM : LEXICOGRAPHICALLY LARGEST ARRAY */
+
+/**  IT IS SIMPLY LEXICOGRAPHIC PROBLEM IN WHICH DATA IS GIVEN IN ARRAY FORMAT
+*   YOU WERE ASKED TO CREATE AN ARRAY S = X + REVERSE OF Y IN WHICH X IS PREFIX OF ARRAY & Y IS POSTFIX
+*   LEN(S)<= LEN(A)
+*   NOTE THAT :  X & Y CAN BE EMPTY
+*
+*/
+
+public class Solution {
+    public int[] solve(int[] A) {
+        
+        if (A.length==1){
+            return A;
+        }
+
+        int last = A[A.length-1];
+        int index=-1;
+        int i=0;
+        
+        while(i < A.length){
+            if (A[i]<last){ index=i; break;}
+            i++;
+        }
+
+        if (index ==-1 ){ return A; }
+
+        int  start=index, stop = A.length-1;
+
+        if(index == A.length-1){ start=0; }
+
+        while(start<stop){
+            int temp = A[start];
+            A[start] = A[stop];
+            A[stop] = temp;
+
+            stop--;
+            start++;
+        }
+
+         return A;
+    }
+}
+
